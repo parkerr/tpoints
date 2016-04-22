2a8febbf1fd5e1a336b24249fc484c94ea8ac2b9
Richard Parker <rlparker.uk@gmail.com>
2016-04-22 14:29:28 +0100

    Add request module for internal API

--- app_server/controllers/cards.js
+++ app_server/controllers/cards.js
@@ -1,3 +1,10 @@
+var request = require('request');
+var apiOptions = {server : "http://localhost:3000"};
+
+  if (process.env.NODE_ENV == 'production'){
+    apiOptions.server = "https://trapoints-demo-2016.herokuapp.com";
+  }
+
 module.exports.cardList = function(req, res){
 	res.render('card-list', { 
     title: 'Active Cards',
