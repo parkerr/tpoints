12ec2c4cbdc847d376c648d4a96863444afa317a
Richard Parker <rlparker.uk@gmail.com>
2016-05-10 22:42:21 +0100

    Allow CORS

--- app.js
+++ app.js
@@ -26,10 +26,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  next();
-});
+  
+  if ('OPTIONS' == req.method) {
+        res.send(200);
+      }
+      else {
+        next();
+      };
+    });
 
 app.use('/', routes);
 app.use('/api', routesApi);
